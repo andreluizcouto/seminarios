1189e25547b1191fbeab41cd35881df16bfcd5e1
andreluizcouto <andrecoutolima@gmail.com>
2025-10-03 09:33:57 -0300

    mudancas necessarias

---
+++ cedo-02/hello-world/src/componentes/PrimeiroComp.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+    function PrimeiroComp(){
+        return(
+            <div>
+                <h2>Meu primeiro componente! (2+2)</h2>
+            </div>
+        )
+    }
+export default PrimeiroComp
--- relembrando/03-JAVASCRIPT/02-primeiro-javascript-script-externo/js/script.js
+++
@@ -1,9 +0,0 @@
-function calcular(){
-
-document.getElementById('demo').innerHTML = 'Hello JavaScript!'
-}
-
-
-function alertar(){
-    window.alert("Hello World")
-}
